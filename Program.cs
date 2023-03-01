8365af0d8b6eed61e8b0de06579f72f03224a36c
Lei Chen <jakey.chen@xsunt.com>
2023-03-01 15:47:03 +0800

    1

--- Program.cs
+++ Program.cs
@@ -69,12 +69,18 @@ namespace CalculatorProgram
                 catch (Exception e)
                 {
                     Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
                     Console.Write("Your option? ");
                 }
 
                 Console.WriteLine("------------------------\n");
 
                 // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
